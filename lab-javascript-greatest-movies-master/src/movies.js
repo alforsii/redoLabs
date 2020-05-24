57f80995339f0f524d58f9b0608e784efe2d6932
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-05-24 11:54:43 -0400

    Iteration 1 - 7 are done

--- lab-javascript-greatest-movies-master/src/movies.js
+++ lab-javascript-greatest-movies-master/src/movies.js
@@ -1,16 +1,75 @@
 // Iteration 1: All directors? - Get the array of all directors.
+// const testArr = [
+//     {
+//       title: 'Paths of Glory',
+//       year: 1957,
+//       director: 'Stanley Kubrick',
+//       duration: '1h 28min',
+//       genre: ['Drama', 'War'],
+//       rate: 8.4
+//     },
+//     {
+//       title: 'Django Unchained',
+//       year: 2012,
+//       director: 'Quentin Tarantino',
+//       duration: '2h 45min',
+//       genre: ['Drama', 'Western'],
+//       rate: 8.4
+//     }
+//   ];
+const getAllDirectors = (movies) => {
+    return { movies: [...movies].map(movie => movie.director)}
+} 
+// const movies = getAllDirectors(movies)
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
+const howManyMovies = (arr) => {
+    return arr.filter(
+        movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+    ).length
+}
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+const ratesAverage = (arr) => {
+    if(arr.length === 0 ) {
+        return 0
+    }
+
+    const len = arr.length
+    const rates = [...arr].map(movie => movie.rate).filter(movie => movie)
+
+    const total = rates.reduce((acc, val) => acc + val) / len
+
+    return Number(total.toFixed(2))
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+const dramaMoviesRate = (arr) => {
+    const rates = [...arr].filter(movie => movie.genre.includes('Drama')).map(
+        movie => movie.rate
+    )
+
+    const total = rates.reduce((acc, val) => acc + val, 0) / rates.length || 0
+
+    return Number(total.toFixed(2))
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+const orderByYear = (arr) => {
+    return [...arr].sort((a,b) => a.year > b.year ? 1 : -1)
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically = (arr) => {
+    const sortedAlphabetic =  [...arr].sort((a,b) => a.title > b.title ? 1 : -1).map(
+        movie => movie.title
+    )
+    return sortedAlphabetic.length > 20 ? sortedAlphabetic.slice(0,20) : sortedAlphabetic 
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+const turnHoursToMinutes = () => {
+    
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
